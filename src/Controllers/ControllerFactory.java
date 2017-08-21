73d5ef5b6ed098463bb139aac7d2b343a3e8d597
Muhammad Ali Jamil <ali.jamil@RT-LT-248.rolustech.net>
2017-08-21 15:35:56 +0500

    Second Commit

--- src/Controllers/ControllerFactory.java
+++ src/Controllers/ControllerFactory.java
@@ -1,34 +1,42 @@
 package Controllers;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import Views.StartView;
 
 public class ControllerFactory 
 {
+	private interface Factory
+	{
+		StandardController create();
+	}
+	
+	private static final Map<String, Factory> factoryMap =Collections.unmodifiableMap(new HashMap<String, Factory>()
+	{{
+		put("Student", new Factory(){ public StandardController create() { return new StudentController();}});
+		put("Teacher", new Factory(){ public StandardController create() { return new TeacherController();}});
+		put("Courses", new Factory(){ public StandardController create() { return new CourseController();}});
+		put("StudentsCourses", new Factory(){ public StandardController create() { return new StudentsCoursesController();}});
+		put("TeachersCourses", new Factory(){ public StandardController create() { return new TeachersCoursesController();}});
+
+	}});
+	
 	public static StandardController getInstance()
 	{
 		String typeName = StartView.startMenu();
 		System.out.println();
-		if(typeName.equals("Student"))
+
+		Factory fact = factoryMap.get(typeName);
+		
+		if(fact!=null)
 		{
-			return new StudentController(); 
-		} 
-		else if(typeName.equals("Teacher"))
-		{
-			return new TeacherController(); 
+			return fact.create();
 		}
-		else if(typeName.equals("Courses"))
-		{
-			return new CourseController(); 
-		}
-		else if(typeName.equals("StudentsCourses"))
-		{
-			return new StudentsCoursesController(); 
-		}
-		else if(typeName.equals("TeachersCourses"))
-		{
-			return new TeachersCoursesController(); 
-		}
+		
 		return null;
 	}
 
 }
+
